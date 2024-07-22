5b92d101c54c39b82a0bb192f33773fca95f9289
doongyeop <leedy977@gmail.com>
2024-07-22 16:00:14 +0900

    fix : 세부 카테고리 jpa repo 참조 엔티티 수정

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/category/repository/CategoryLowRepository.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/category/repository/CategoryLowRepository.java
@@ -1,7 +1,7 @@
 package com.ssafy.ozz.clothes.category.repository;
 
-import com.ssafy.ozz.clothes.category.domain.CategoryHigh;
+import com.ssafy.ozz.clothes.category.domain.CategoryLow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryLowRepository extends JpaRepository<CategoryHigh, Long> {
+public interface CategoryLowRepository extends JpaRepository<CategoryLow, Long> {
 }
