26d60b0c5c9945ade0a9fc395055eeb4ce0e1819
doongyeop <leedy977@gmail.com>
2024-07-22 14:33:20 +0900

    feat : 옷 관련 entity 추가

---
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/domain/Clothes.java
@@ -0,0 +1,66 @@
+package com.ssafy.ozz.clothes.clothes.domain;
+
+import com.ssafy.ozz.clothes.category.domain.CategoryLow;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Table
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class Clothes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long clothesId;
+
+    @Column(columnDefinition = "BIT(32)")
+    private Integer color;
+
+    @Column(length = 255)
+    private String name;
+
+    @Column
+    private Byte size;
+
+    @Column
+    private Byte fit;
+
+    @Column(columnDefinition = "BIT(20)")
+    private Integer texture;
+
+    @Column(length = 255)
+    private String memo;
+
+    @Column(length = 50)
+    private String brand;
+
+    @Column
+    private LocalDateTime purchaseDate;
+
+    @Column(length = 100)
+    private String purchaseSite;
+
+    @Column(length = 100)
+    private LocalDateTime createDate;
+
+    @Column(columnDefinition = "BIT(10)")
+    private Short style;
+
+    @Column(columnDefinition = "BIT(4)")
+    private Byte season;
+
+    /* FOREIGN KEY */
+    @Column
+    Long imageFileId;
+
+    @ManyToOne
+    @JoinColumn(name="category_low_id")
+    CategoryLow categoryLow;
+
+    @Column
+    Long userId;
+}
